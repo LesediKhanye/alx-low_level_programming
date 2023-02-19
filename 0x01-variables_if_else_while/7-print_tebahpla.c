2dab877c6b5e77058e9641f94535e9433920254a
Lesedi Khanye <khanye.l22@gmail.com>
2023-02-19 02:47:45 -0800

    Print alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lowercase from z to a
+ * followed by a newline
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
