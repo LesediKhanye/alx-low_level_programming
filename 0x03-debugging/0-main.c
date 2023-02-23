d64c1fd1b4077e2a0c5656637c2daf8e2c8f7dc8
Lesedi Khanye <khanye.l22@gmail.com>
2023-02-23 11:14:04 -0800

    0-main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,9 +8,19 @@
 int main(void)
 {
         int i;
+	
+	printf("Infinite loop incoming :(\n");
 
-        i = 98;
-        positive_or_negative(i);
+	i = 0;
 
-        return (0);
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+
+
 }
